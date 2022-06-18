71d8555c9f171015a895d8aa8e8d731bc3c12bf7
amita <amitasher4@gmail.com>
2022-06-18 19:56:39 +0300

    sdsda

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-
-import './App.css';
+import "./App.css";
 //// components
-import SectionOne from './components/Section1/SectionOne';
-import SectionTwo from './components/section2/SectionTwo';
-import SectionTree from './components/Section3/SectionTree';
-import SectionFour from './components/section4/SectionFour';
-import SectionCountect from './components/contectUs/SectionCountect';
+import SectionOne from "./components/Section1/SectionOne";
+import SectionTwo from "./components/section2/SectionTwo";
+import SectionTree from "./components/Section3/SectionTree";
+import SectionFour from "./components/section4/SectionFour";
+import SectionCountect from "./components/contectUs/SectionCountect";
 
-
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 AOS.init();
-
 
 function App() {
   return (
     <div className="App">
-    <SectionOne/>
-    <SectionFour/>
-    <SectionTwo/>
-    <SectionTree/>
-    <SectionCountect/>
-   
+      <SectionOne />
+      <SectionFour />
+      <SectionTwo />
+      <SectionTree />
+      <SectionCountect />
     </div>
   );
 }
